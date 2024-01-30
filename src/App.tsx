19572e6e9e84bf315ec217b822e1135a40d1f69b
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-29 21:49:28 -0300

    Criação das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
 import * as React from 'react';
 import {ThemeProvider, Box} from "@mui/system";
 import {Header} from "./components/Header";
-import {createTheme} from "@mui/material";
 import {Layout} from "./components/Layout";
+import {appTheme} from "./config/theme";
+import { Routes, Route, Link } from 'react-router-dom';
+import Typography from "@mui/material/Typography";
 
-const theme = createTheme({});
+const Home = () => (
+    <Box>
+        <Typography variant='h3' component='h1'>
+            Home
+        </Typography>
+    </Box>
+);
+
+const About = () => (
+    <Box>
+        <Typography variant='h3' component='h1'>
+            About
+        </Typography>
+    </Box>
+);
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={appTheme}>
             <Box component='main' sx={{
                 height: '100vh',
-                background: '#000',
+                background: (theme) => theme.palette.grey[100],
             }}>
                 <Header />
                 <Layout>
-                    <h1>Olá mundo</h1>
+                    <h1>Welcome to React Router</h1>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/about' element={<About />} />
+                    </Routes>
                 </Layout>
             </Box>
         </ThemeProvider>
