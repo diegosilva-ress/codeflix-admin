6200446816ca492c893d65607a22c703106625ea
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-29 21:13:42 -0300

    Criação dos primeiros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import {ThemeProvider, Box} from "@mui/system";
+import {Header} from "./components/Header";
+import {createTheme} from "@mui/material";
+import {Layout} from "./components/Layout";
+
+const theme = createTheme({});
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+    return (
+        <ThemeProvider theme={theme}>
+            <Box component='main' sx={{
+                height: '100vh',
+                background: '#000',
+            }}>
+                <Header />
+                <Layout>
+                    <h1>Olá mundo</h1>
+                </Layout>
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
