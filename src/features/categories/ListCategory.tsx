017d5bf767cc143e45a6ef970c81515d0966698e
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-02-05 21:38:55 -0300

    Customizando datagrid

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -3,22 +3,44 @@ import {Link} from "react-router-dom";
 import * as React from "react";
 import {useAppSelector} from "../../app/hooks";
 import {selectCategories} from "./categorySlice";
-import {DataGrid, GridColDef, GridRowsProp} from '@mui/x-data-grid';
+import {DataGrid, GridColDef, GridRenderCellParams, GridRowsProp, renderActionsCell} from '@mui/x-data-grid';
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from '@mui/icons-material/Delete'
 
 export const CategoryList = () => {
     const categories = useAppSelector(selectCategories);
 
-    const rows : GridRowsProp = categories.map((category) => ({
+    const rows: GridRowsProp = categories.map((category) => ({
         id: category.id,
         name: category.name,
-        description: category.description,
+        isActive: category.is_active,
+        createdAt: new Date(category.created_at).toLocaleDateString('pt-BR'),
     }))
 
-    const columns : GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 150 },
-        { field: 'name', headerName: 'Name', width: 150 },
-        { field: 'description', headerName: 'Description', width: 150 }
+    const columns: GridColDef[] = [
+        {field: 'name', headerName: 'Name', flex: 1},
+        {field: 'isActive', headerName: 'Active', flex: 1, type: 'boolean', renderCell: renderIsActiveCell},
+        {field: 'createdAt', headerName: 'CreatedAt', flex: 1},
+        {field: 'id', headerName: 'Actions', flex: 1, renderCell: renderActionsCell},
     ]
+
+    function renderIsActiveCell(row: GridRenderCellParams) {
+        return (
+            <Typography color={row.value ? 'primary' : 'secondary'}>
+                {row.value ? 'Active' : 'Inactive'}
+            </Typography>
+        );
+    }
+
+    function renderActionsCell(params: GridRenderCellParams) {
+        return (
+            <IconButton color='secondary'
+                        onClick={() => console.log('clicked')}
+                        aria-label='delete'>
+                <DeleteIcon />
+            </IconButton>
+        );
+    }
 
     return (
         <Box maxWidth="lg" sx={{mt: 4, mb: 4}}>
@@ -33,10 +55,10 @@ export const CategoryList = () => {
                     New Category
                 </Button>
             </Box>
-            <div style={{ height: 300, width: '100%'}}>
+            <div style={{height: 300, width: '100%'}}>
                 <DataGrid
                     rows={rows}
-                    columns={columns} />
+                    columns={columns}/>
             </div>
         </Box>
     );
